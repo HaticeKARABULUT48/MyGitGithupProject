655d34575dff3114a8bfccfdc0a262b6e0cf5d9e
hatice <hatice.kbulut.48@gmail.com>
2023-04-26 21:52:32 +0300

    v1

---
+++ src/AtmApp.java
@@ -0,0 +1,10 @@
+public class AtmApp {
+    public static void main(String[] args) {
+
+        System.out.println("bu bir atm projesidir..");
+        //version 1
+        System.out.println("");
+
+
+    }
+}
